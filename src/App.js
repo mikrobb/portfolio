ac4bd9ac1c16841f060f6f182b8c8f63012f6ff3
mikrob <promikrob@gmail.com>
2022-10-02 16:30:37 +0300

    add new page and some fix bugs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,70 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Cards/MainHomePage";
+import SkillsPage from "./Cards/SkillsPage";
+import { useState } from "react";
+
+function setToLocalStorage(key, value) {
+  return localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getFromLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
 
 function App() {
+  const [theme, setTheme] = useState(getFromLocalStorage("theme") || "light");
+
+  if (getFromLocalStorage("theme").includes("light")) {
+    document.getElementById("bodyMain").style.backgroundColor = "white";
+  } else {
+    document.getElementById("bodyMain").style.backgroundColor = "#2a2a2a";
+  }
+
+  function switcherBlack() {
+    setTheme("black");
+    setToLocalStorage("theme", theme);
+    console.log(theme);
+    document.getElementById("bodyMain").style.backgroundColor = "#2a2a2a";
+  }
+
+  function switcherLight() {
+    setTheme("light");
+    setToLocalStorage("theme", theme);
+    console.log(theme);
+    document.getElementById("bodyMain").style.backgroundColor = "white";
+  }
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <HomePage
+                setToLocalStorage={setToLocalStorage}
+                getFromLocalStorage={getFromLocalStorage}
+                theme={theme}
+                setTheme={setTheme}
+                switcherBlack={switcherBlack}
+                switcherLight={switcherLight}
+              />
+            }
+          />
+          <Route
+            path="/skillPage"
+            element={
+              <SkillsPage
+                setToLocalStorage={setToLocalStorage}
+                getFromLocalStorage={getFromLocalStorage}
+                theme={theme}
+                setTheme={setTheme}
+                switcherBlack={switcherBlack}
+                switcherLight={switcherLight}
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
--- src/Cards/SkillsPage.js
+++ src/Cards/SkillsPage.js
@@ -1,9 +1,8 @@
 import React from "react";
-import "../CardsCss/MainHomePage.css";
-import img from "../img/myPhoto.jpg";
 import { Link } from "react-router-dom";
+import "../CardsCss/SkillsPage.css";
 
-export default function HomePage({
+export default function SkillsPage({
   getFromLocalStorage,
   theme,
   switcherBlack,
@@ -11,7 +10,7 @@ export default function HomePage({
 }) {
   return (
     <>
-      <header className="header">
+      <header>
         <div className="divHeader">
           <div>
             <Link
@@ -32,7 +31,6 @@ export default function HomePage({
                   ? "divHeader_blocks"
                   : "divHeader_blocksAnother"
               }
-              to="skillPage"
             >
               Skills
             </Link>
@@ -63,41 +61,8 @@ export default function HomePage({
           </div>
         </div>
       </header>
-      <section className="sectionMain">
-        <div className="sectionMain_Block">
-          <div className="sectionBlock">
-            <div className="sectionBlock_message">Hello! I am</div>
-            <div
-              className={
-                getFromLocalStorage("theme").includes("light")
-                  ? "sectionBlock_Name"
-                  : "sectionBlock_Name_Another"
-              }
-            >
-              Andrey Mechtanov
-            </div>
-            <div className="sectionBlock_Dev">Front-end developer</div>
-            <div className="sectionBlock_Links">
-              <div className="sectionBlock_Links_Link">
-                <a href="https://www.linkedin.com/in/andrey-mechtanov-1b21bb22b/">
-                  LinkedIn
-                </a>
-              </div>
-              <div className="sectionBlock_Links_Link">
-                <a href="https://github.com/mikrobb">GitHub</a>
-              </div>
-              <div className="sectionBlock_Links_Link">
-                <a href="https://t.me/Mikrob_b">Telegram</a>
-              </div>
-              <div className="sectionBlock_Links_Link">
-                <a href="https://www.instagram.com/andrysha_666_/">Instagram</a>
-              </div>
-            </div>
-          </div>
-          <div className="sectionImg">
-            <img className="sectionImg_Photo" src={img} alt="" />
-          </div>
-        </div>
+      <section className="skillsSection">
+        <h1 className="titleSkills">Skills</h1>
       </section>
       <footer>
         <div>
