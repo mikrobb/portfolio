46e722c1fe889dd537db9e30f9d30e658e5be013
mikrob <promikrob@gmail.com>
2022-10-05 16:39:16 +0300

    start contact form

--- src/Cards/ContactPage.js
+++ src/Cards/ContactPage.js
@@ -12,7 +12,51 @@ export default function ContactPage({
   return (
     <>
       <Header getFromLocalStorage={getFromLocalStorage} />
-      <section></section>
+      <section>
+        <h1
+          className={
+            getFromLocalStorage("theme").includes("light")
+              ? "titleSkills"
+              : "titleSkills_Another"
+          }
+        >
+          Contact Me
+        </h1>
+        <form
+          action="https://app.form2chat.io/f/17133913"
+          id="contact-form"
+          method="POST"
+          accept-charset="UTF-8"
+          className="formContact"
+        >
+          <input
+            className="inputContact"
+            type="text"
+            name="name"
+            placeholder="Type Your Name"
+          />
+          <br />
+          <input
+            className="inputContact"
+            type="email"
+            name="email"
+            placeholder="Type Your Email"
+          />
+          <br />
+          <textarea
+            className="textAreaContect"
+            cols="30"
+            rows="10"
+            placeholder="Some Text"
+            name="text"
+          ></textarea>
+          <br />
+          <button className="buttonContact" type="submit">
+            Send
+          </button>
+          <div id="submit-result"></div>
+        </form>
+      </section>
       <Footer
         getFromLocalStorage={getFromLocalStorage}
         theme={theme}
